de195692bf29d6d4ed7f02c6d9dab5ad46c310e5
Polarblau <polarblau@gmail.com>
2013-04-19 16:44:24 +0300

    Activating live reload.

--- config.rb
+++ config.rb
@@ -7,25 +7,25 @@ set :js_dir,          'javascripts'
 set :images_dir,      'images'
 
 # Livereload
-# activate :livereload
+activate :livereload
 
 # Build-specific configuration
 configure :build do
-  activate :minify_css  
+  activate :minify_css
   activate :minify_javascript
   activate :gzip
-  
+
   # Enable cache buster
   # activate :cache_buster
-  
+
   # Use relative URLs
   # activate :relative_assets
-  
+
   # Compress PNGs after build
   # First: gem install middleman-smusher
   # require "middleman-smusher"
   # activate :smusher
-  
+
   # Or use a different image path
   # set :http_path, "/Content/images/"
 end
